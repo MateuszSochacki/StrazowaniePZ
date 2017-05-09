08be691a30222002ee91d950788047b138f271ec
Wilku123 <bjarni123@gmail.com>
2017-05-09 21:32:11 +0200

    Griddo panello is finally wórking! Filter checkbox works only for genre tho griddo pannelo should be clicable but it isnt yet

--- src/main/java/com/cinema/controller/ChooseMovieController.java
+++ src/main/java/com/cinema/controller/ChooseMovieController.java
@@ -4,21 +4,31 @@ import com.cinema.config.BootInitializable;
 import com.cinema.model.AgeRatingEntity;
 import com.cinema.model.CategoryEntity;
 import com.cinema.model.MovieEntity;
+import com.cinema.model.MovieHasCategoryEntity;
 import com.cinema.services.AgeRatingRepository;
 import com.cinema.services.CategoryRepository;
+import com.cinema.services.MovieHasCategoryRepository;
 import com.cinema.services.MovieRepository;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.springframework.beans.BeansException;
@@ -26,11 +36,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 /**
  * Created by xareen on 04.05.2017.
@@ -39,7 +53,7 @@ import java.util.ResourceBundle;
 public class ChooseMovieController implements BootInitializable {
 
     @FXML
-    private GridPane gridPaneMovie;
+    private ScrollPane scrollPanelMovie;
     @FXML
     private ComboBox<Integer> comboBoxAge;
     @FXML
@@ -54,19 +68,90 @@ public class ChooseMovieController implements BootInitializable {
     private Label labelAge;
     @FXML
     private TextArea textAreaDescription;
+    GridPane gridPaneMovie = new GridPane();
+    List<MovieEntity> movieList;
 
     @Autowired
     private MovieRepository movieRepository;
-
     @Autowired
     private AgeRatingRepository ageRatingRepository;
-
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private MovieHasCategoryRepository movieHasCategoryRepository;
 
     private ApplicationContext springContext;
 
-    public void setFields() {
+
+    public void setDataGrid(int listSize)
+    {
+        //gridPaneMovie.setGridLinesVisible(true);
+        scrollPanelMovie.setContent(gridPaneMovie);
+        RowConstraints templateRow = new RowConstraints();
+        templateRow.setPrefHeight(250);
+        gridPaneMovie.getRowConstraints().add(templateRow);
+        gridPaneMovie.setAlignment(Pos.CENTER);
+
+        for (int i =0;i<4;i++)
+        {
+            ColumnConstraints column = new ColumnConstraints();
+            column.setPercentWidth(25); //180 kazda z nich
+            column.setHalignment(HPos.CENTER);
+            gridPaneMovie.getColumnConstraints().add(column);
+        }
+
+        for (int i=0;i<listSize%3;i++)
+        {
+            RowConstraints row = new RowConstraints();
+            row.setPrefHeight(250);
+            row.setValignment(VPos.CENTER);
+            gridPaneMovie.getRowConstraints().add(row);
+
+        }
+
+    }
+    public void fillDataGrid(List<MovieEntity> movieEntityList)
+    {
+        int col = 0;
+        int r =0;
+        for (MovieEntity i:movieEntityList)
+        {
+
+            StackPane panel = new StackPane();
+            panel.setPadding(new Insets(10,20,20,20));
+            ImageView imgView = new ImageView();
+            imgView.setFitWidth(140);
+            imgView.setFitHeight(190);
+            byte[] byteArray = i.getCover();
+            ByteArrayInputStream in = new ByteArrayInputStream(byteArray);
+            BufferedImage read = null;
+            try {
+                read = ImageIO.read(in);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            imgView.setImage(SwingFXUtils.toFXImage(read, null));
+            panel.setAlignment(imgView,Pos.TOP_CENTER);
+            Label label = new Label();
+            label.setText(i.getTitle());
+            //imgView.setImage(i.getCover());
+            panel.getChildren().add(imgView);
+            panel.setAlignment(label,Pos.BOTTOM_CENTER);
+            panel.getChildren().add(label);
+            //movieList.add(i.getTitle());
+            //label.setText(i.getTitle());
+            //gridPaneMovie.add(label,col,r);
+            if (col ==4){
+                col=0;
+                r++;
+            }
+            gridPaneMovie.add(panel,col,r);
+            col++;
+
+        }
+    }
+    public void fillComboBox()
+    {
         List<Integer> ageRatingList = new ArrayList<>();
         for (AgeRatingEntity i:ageRatingRepository.findAll())
         {
@@ -86,23 +171,35 @@ public class ChooseMovieController implements BootInitializable {
         comboBoxGenre.setItems(categoryEntityObservableList);
 
         comboBoxDuration.getItems().addAll(">1h","1h<","2h<");
-        ImageView imgView = new ImageView();
+    }
 
-        gridPaneMovie.add(imgView,0,0);
+    public void filterDataGridByGenre()
+    {
+        labelGenre.setText(comboBoxGenre.getValue());
+        comboBoxGenre.valueProperty().addListener(new ChangeListener<String>() {
+            @Override public void changed(ObservableValue ov, String t, String t1) {
+//                System.out.println(ov); Object
+//                System.out.println(t); Poprzednio wybrany
+//                System.out.println(t1); Nowo wybrany
+                CategoryEntity category = categoryRepository.findAll().stream().filter(categoryEntity -> categoryEntity.getName().equals(t1)).findAny().get();
+                List<MovieHasCategoryEntity> movieListAfterFilterById = movieHasCategoryRepository.findAll().stream().filter(movieHasCategoryEntity -> movieHasCategoryEntity.getCategoryIdCategory().equals(category.getIdCategory())).collect(Collectors.toList());
+                List<MovieEntity> movieListAfterFilter= new ArrayList<>();
+                for (MovieHasCategoryEntity i :movieListAfterFilterById) {
+                    movieListAfterFilter.addAll(movieList.stream().filter(movieEntity -> movieEntity.getIdMovie().equals(i.getMovieIdMovie())).collect(Collectors.toList()));
+                }
+                gridPaneMovie.getChildren().clear();
+                setDataGrid(movieListAfterFilter.size());
+                fillDataGrid(movieListAfterFilter);
+            }
+        });
+    }
 
-
-
-
-
-
-
-
-
-
-
+    public void setFields() {
 
 
     }
+
+
     @Override
     public void initConstruct() {
     }
@@ -132,7 +229,12 @@ public class ChooseMovieController implements BootInitializable {
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+        movieList = movieRepository.findAll();
         setFields();
+        setDataGrid(movieList.size());
+        fillDataGrid(movieList);
+        fillComboBox();
+        filterDataGridByGenre();
     }
 
     @Override
@@ -142,5 +244,9 @@ public class ChooseMovieController implements BootInitializable {
 
     public void onAgeChanged(ActionEvent actionEvent){
 
+    }
+    @FXML
+    private void genreChange(ActionEvent event)
+    {
     }
 }
--- src/main/java/com/cinema/services/MovieHasCategoryRepository.java
+++ src/main/java/com/cinema/services/MovieHasCategoryRepository.java
@@ -3,8 +3,12 @@ package com.cinema.services;
 import com.cinema.model.MovieHasCategoryEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by msoch_000 on 02-05-2017.
  */
 public interface MovieHasCategoryRepository extends CrudRepository<MovieHasCategoryEntity, Integer> {
+    @Override
+    List<MovieHasCategoryEntity> findAll();
 }
